f428c13fbae2ba480a5ce54a5c052a47439a2783
Cisco Wicaksana <cisco@wicaksana.org>
2024-05-23 00:43:01 +0700

    fixed models,, updated DB

--- jobs/models.py
+++ jobs/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Job(models.Model):
     job_name = models.CharField(max_length=100)
     publication_date = models.DateField()
-    location = models.CharField(max_length=100)
+    job_location = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
-    source_site = models.CharField(max_length=50)
-    job_link = models.URLField()
+    source = models.CharField(max_length=50)
+    source_url = models.URLField()
 
     def __str__(self):
         return self.job_name
